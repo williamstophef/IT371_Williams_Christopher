f10fe05d8274013cd360966c5009fa006d08d87f
williamstophef <104956463+williamstophef@users.noreply.github.com>
2022-06-19 18:30:41 -0400

    Delete store.js

--- store.js
+++
@@ -1,9 +0,0 @@
-var removeCartItemButtons = document.getElementsByClassName("button")
-console.log(removeCartItemButtons)
-for (var i = 0; i < removeCartItemButtons.length; i++){
-  var button = removeCartItemButtons[i]
-  button.addEventListener('click', function(event){
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-  })
-}
